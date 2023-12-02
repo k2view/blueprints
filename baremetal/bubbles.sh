e99e62b2316a183c6c20b7ee505a857e6cbead10
Nader Ganayem <nader.ganayem@prospera.ag>
2023-12-03 00:49:02 +0200

    replace gum with bash functions

---
+++ baremetal/bubbles.sh
@@ -0,0 +1,215 @@
+#!/bin/bash
+
+# Function to split command line parameters into two groups:
+# one group for parameters before '--' and another for those after.
+# Usage: split_parameters param1 param2 -- param3 param4
+split_parameters() {
+    # Arrays to hold parameters before and after the delimiter '--'
+    local params_before=()
+    local params_after=()
+
+    # Flag to indicate if the delimiter '--' has been found
+    local delimiter_found=0
+
+    # Iterate over each passed argument
+    for param in "$@"; do
+        # Check if the current parameter is the delimiter
+        if [[ $param == "--" ]]; then
+            delimiter_found=1  # Set the flag indicating delimiter found
+            continue          # Skip further processing for delimiter
+        fi
+
+        # Add the parameter to the appropriate array
+        if [[ $delimiter_found -eq 0 ]]; then
+            params_before+=("$param")  # Add to params_before if delimiter not found
+        else
+            params_after+=("$param")   # Add to params_after if delimiter found
+        fi
+    done
+
+    # Combine parameters from each array into space-separated strings
+    before_dashes="${params_before[*]}"
+    after_dashes="${params_after[*]}"
+}
+
+################################################################
+###
+################################################################
+confirm() {
+    local message=$1  # Message to be displayed above the buttons
+
+    # Function to draw a button
+    # Arguments: x-position, y-position, label, focus
+    draw_button() {
+        local x=$1
+        local y=$2
+        local label=$3
+        local focus=$4
+
+        # Set focus color
+        if [ "$focus" -eq 1 ]; then
+            tput setaf 14 # Set foreground color to violet
+	        tput setab 255 # Set background color to white
+            tput rev      # Reverse video mode for standout effect
+        fi
+
+        # Draw the button
+        tput cup $((y + 0)) $x  # Adjust y position for message
+        echo -n " $label "
+
+        # Reset text attributes
+        tput sgr0
+    }
+
+    # Initialize
+    tput civis  # Hide cursor
+
+    # Display the message in green
+    tput setaf 2  # Set text color to green
+    echo "$message"
+    # tput sgr0     # Reset text attributes
+
+    # Calculate positions for buttons
+    local lines_above=$(( $(tput lines) - 0 ))
+    local confirm_button_y=$((lines_above + 1))
+    local cancel_button_y=$((lines_above + 1))
+
+
+    # Initial focus
+    local focus=1
+
+    # Main loop
+    while true; do
+        # Draw buttons
+        draw_button 10 $confirm_button_y "Confirm" $((focus == 1))
+        draw_button 20 $cancel_button_y "Cancel" $((focus == 2))
+
+        # Read user input
+        IFS= read -rsn1 input
+
+        case $input in
+            $'\e')  # Arrow key prefix
+                # Read the rest of the escape sequence
+                read -rsn2 input
+                case $input in
+                    '[C')  # Right arrow
+                        [ "$focus" -eq 1 ] && focus=2
+                        ;;
+                    '[D')  # Left arrow
+                        [ "$focus" -eq 2 ] && focus=1
+                        ;;
+                esac
+                ;;
+            "")  # Enter key
+                # Return 0 for Confirm, 1 for Cancel
+                if [ "$focus" -eq 1 ]; then
+                    tput cnorm  # Show cursor
+                    echo; echo
+                    return 0
+                else
+                    tput cnorm  # Show cursor
+                    echo; echo
+                    return 1
+                fi                ;;
+            *)  # Ignore other inputs
+                ;;
+        esac
+    done
+    # Cleanup
+    tput cnorm  # Show cursor
+}
+
+
+# Function to print text in a given color and bold
+# $1 - Color (red, green, blue, yellow, etc.)
+# $2 - Text to print
+# 
+# Example usage
+# print_colored_bold "red" "This is bold and red text"
+# print_colored_bold "blue" "This is bold and blue text"
+print_colored_bold() {
+    local color=$1; shift
+    local text=$*
+    local color_code=""
+
+    # Determine color code
+    case $color in
+        black) color_code='\033[0;30m' ;;
+        red) color_code='\033[0;31m' ;;
+        green) color_code='\033[0;32m' ;;
+        yellow) color_code='\033[0;33m' ;;
+        blue) color_code='\033[0;34m' ;;
+        magenta) color_code='\033[0;35m' ;;
+        cyan) color_code='\033[0;36m' ;;
+        white) color_code='\033[0;39m' ;;
+        *) echo "Invalid color"; return 1 ;;
+    esac
+    
+    # Print text in bold and the specified color
+    printf "${color_code}\033[1m${text}\033[0m"
+}
+
+# Function to display a message and a spinner
+# Usage: spinner "Your message here..." -- "command"
+spinner() {
+
+    start_spinner() {
+        local fgcolor=${BUBBLES_FG_COLOR:-"white"}
+        local message=$1
+        local pid=$2
+        local delay=0.1
+        local spinstr='|/-\'
+
+        tput civis
+
+        print_colored_bold "$fgcolor" "$message"
+        while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+            local temp=${spinstr#?}
+            printf " [%c]  " "$spinstr"
+            local spinstr=$temp${spinstr%"$temp"}
+            sleep $delay
+            printf "\b\b\b\b\b\b"
+        done
+        printf "    \b\b\b\b"
+        tput cnorm
+    }
+
+    split_parameters $@
+    message=$before_dashes
+    command=$after_dashes
+    ( $command & echo $! > /tmp/bg_job.pid )
+    pid=$(cat /tmp/bg_job.pid)
+    start_spinner "$message" $pid
+    echo ""
+}
+
+#!/bin/bash
+
+print_in_box() {
+    local color="$1"
+    local text="$2"
+    local length=${#text}
+    local padding=$(printf '%*s' "$length")
+
+    # Box-drawing characters
+    local upper_left_corner='┌'
+    local upper_right_corner='┐'
+    local lower_left_corner='└'
+    local lower_right_corner='┘'
+    local vertical_line='│'
+
+    # Print the top border
+    print_colored_bold "$color"  "${upper_left_corner}${padding// /─}${upper_right_corner}\n"
+
+    # Print an empty line above the text
+    print_colored_bold "$color" "${vertical_line}${padding}${vertical_line}\n"
+
+    # Print the text within borders
+    print_colored_bold "$color" "${vertical_line}${text}${vertical_line}\n"
+
+    # Print an empty line below the text
+    print_colored_bold "$color" "${vertical_line}${padding}${vertical_line}\n"
+
+    # Print the bottom border
+    print_colored_bold "$color" "${lower_left_corner}${padding// /─}${lower_right_corner}\n"
+}
---
+++ baremetal/single_node.sh
@@ -0,0 +1,265 @@
+#!/bin/bash
+
+source ./bubbles.sh
+
+function init() {
+    DISTRIBUTION="Unknown"
+
+    case "$(uname -s)" in
+        Linux*)     OS=linux
+    		            print_colored_bold "green" "Installing $OS dependencies completed ..."
+		                ;;
+	
+        Darwin*)    OS=MacOS ;;
+
+        *)      OS="Unknown" ;;
+    esac  
+    print_colored_bold "green" "Initializing Completed! "
+}
+
+
+function install_git(){
+ if git version &> /dev/null
+ then
+  print_colored_bold "green" '√ Git Source Control Already Installed ...'
+ else
+   case "$OS" in
+    linux) 
+      BUBBLES_FG_COLOR="green" spinner "Installing Git Source Control ..." -- apt install -y git
+      ;;
+    MacOS) 
+      BUBBLES_FG_COLOR="green" spinner "Installing Git Source Control ..." -- brew install git 
+      ;;
+    esac
+   if git version &> /dev/null
+   then
+        print_colored_bold "green" '√ Git installed successfully ...'
+   else
+        print_colored_bold "red" 'x Git installation failed ...'
+   fi
+ fi
+}
+
+function install_docker() {
+    echo '''
+    exec &>/tmp/install_docker.log 
+    apt update
+    apt install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/docker-archive-keyring.gpg >/dev/null
+    add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt update
+    DEBIAN_FRONTEND=noninteractive apt install -y docker-ce 
+    systemctl enable docker
+    systemctl start docker
+    ''' > /tmp/install_docker.sh
+    chmod +x /tmp/install_docker.sh 
+    BUBBLES_FG_COLOR="green" spinner "Installing Docker ..." -- bash -c "/tmp/install_docker.sh"
+    if docker ps &> /dev/null
+    then
+	   print_colored_bold "green" '√ Docker installed successfully ...' 
+    else
+	   print_colored_bold "red" 'x Docker installation failed ...' 
+    fi
+
+}
+
+function install_microk8s() {
+
+case "$OS" in 
+  linux) 
+	    echo '''
+        #!/bin/bash -x
+        exec &>/tmp/install_microk8s.log 
+        sudo apt update
+        echo "vm.nr_hugepages=1024" | sudo tee -a /etc/sysctl.d/20-microk8s-hugepages.conf
+        sysctl -w vm.nr_hugepages=1024
+        sysctl -p
+        sudo apt-get install -y  linux-modules-extra-$(uname -r)
+        sudo modprobe nvme-tcp
+        echo 'nvme-tcp' | sudo tee -a /etc/modules-load.d/microk8s-mayastor.conf
+        sudo apt install snapd -y
+        snap version
+        sudo snap install microk8s --classic --channel=1.27
+        sudo usermod -a -G microk8s ubuntu
+        sudo chown -f -R ubuntu ~/.kube
+        su - ubuntu
+        microk8s status --wait-ready
+      ''' > /tmp/install_microk8s_linux.sh
+      chmod +x /tmp/install_microk8s_linux.sh
+      
+      BUBBLES_FG_COLOR="green" spinner  "Installing Kubernetes ..." -- bash -c "/tmp/install_microk8s_linux.sh"
+      if microk8s status  | grep -q "microk8s is running" 
+      then
+        print_colored_bold "green" '√ Kubernetes installed successfully ...' 
+      else
+        print_colored_bold "red" 'x Kubernetes installation failed ...' 
+      fi
+      ;;
+  MacOS)
+      if brew list minikube &> /dev/null
+      then
+        minikube start 
+      else
+        brew install --cask docker
+        open /Applications/Docker.app
+        brew install minikube
+        brew install kubectl
+        brew install helm
+        minikube start --kubernetes-version=v1.26.3 
+      fi
+
+      if minikube status  &> /dev/null
+      then
+       print_colored_bold "green" '√ Kubernetes installed successfully ...' 
+      else
+        print_colored_bold "red" 'x Kubernetes installation failed ...' 
+      fi
+    ;;
+  esac
+}
+
+
+function configure_microk8s() {
+
+echo ""
+print_colored_bold "magenta" "The following tools will be installed:"
+echo "\n
+|Name            |Required |More info                                   |
+|----------------|---------|--------------------------------------------|
+|cert-manager    |Yes      |'https://cert-manager.io/'                  |
+|NGINX Ingress   |Yes      |'https://docs.nginx.com/'                   |
+|hostpath-storage|Yes      |                                            |
+|docker registry |Yes      |'https://microk8s.io/docs/registry-built-in'|                           |
+|metrics-server  |Yes      |                                            |
+"
+echo ""
+echo ""
+case "$OS" in 
+  linux) 
+
+      echo '''
+      #!/bin/bash -x
+      exec &>/tmp/configure_microk8s.log
+      microk8s kubectl cluster-info
+      code=0
+      for addon in dns ingress cert-manager storage registry metrics-server
+      do
+        microk8s enable $addon
+        echo  "\t\t Installing ${addon} ..."
+        ((code+=$?))
+      done
+      microk8s kubectl get all --all-namespaces
+      microk8s kubectl get nodes
+      echo $code > /tmp/configure_microk8s.code
+      ''' > /tmp/configure_microk8s.sh
+      chmod +x /tmp/configure_microk8s.sh
+      BUBBLES_FG_COLOR="green" spinner "Configuring Kubernetes ..." -- bash -c "/tmp/configure_microk8s.sh"
+      code=`cat /tmp/configure_microk8s.code`
+
+      if [[ $code -eq 0 ]]
+      then
+        print_colored_bold "green" '√ Kubernetes Configured successfully ...'
+      else
+        print_colored_bold "red" 'x Kubernetes Configuration failed ...'
+      fi
+      ;;
+  MacOS)
+    for addon in ingress metrics-server registry volumesnapshots csi-hostpath-driver
+    do
+      print_colored_bold "cyan" $addon
+      minikube addons enable $addon
+    done
+    ;;
+  esac
+
+}
+
+
+function install_k2agent() {
+  echo; echo
+  print_in_box "cyan" "Installing K2View Agent"
+  echo
+
+  print_colored_bold "cyan" "Enter Mailbox URL: "
+  read MANAGER_URL 
+  print_colored_bold "cyan"  "Enter Mailbox ID: "
+  read MAILBOX_ID
+
+  # MANAGER_URL=`$GUM input --prompt "Enter Mailbox URL: " --prompt.foreground=212 --placeholder "https://cloud-dev.k2view.com/api/mailbox"`
+
+  # MAILBOX_ID=`$GUM input --prompt "Enter Mailbox ID: " --prompt.foreground=212 --placeholder "12345678-0123-0123-0123-123456789012"`
+
+case "$OS" in 
+ linux)
+    echo """
+      #!/bin/bash -x
+      exec &>/tmp/k2_agent_install.log
+      rm -rf blueprints || true
+      git clone https://github.com/k2view/blueprints.git
+      cd blueprints/helm/k2view-agent
+      # microk8s kubectl create namespace k2view-agent
+      helm install k2-agent . --set secrets.K2_MAILBOX_ID=\"$MAILBOX_ID\" --set secrets.K2_MANAGER_URL=\"$MANAGER_URL\"
+      """ > /tmp/k2agent_install.sh
+      chmod +x /tmp/k2agent_install.sh
+      BUBBLES_FG_COLOR="green" spinner "Installing K2View Agent ..." -- bash -c "/tmp/k2agent_install.sh"
+      sleep 5
+      if microk8s kubectl get deploy k2view-agent | grep -q "k2view-agent   1/1"
+      then
+        print_colored_bold "green" '√ K2-Agent Deployed successfully ...'
+      else
+        print_colored_bold "red" 'x K2-Agent Deployment failed ...'
+      fi
+
+      ;;
+  MacOS)
+      rm -rf blueprints || true
+      git clone https://github.com/k2view/blueprints.git
+      cd blueprints/helm/k2view-agent
+      helm uninstall k2-agent &>/dev/null || true
+      sleep 10
+      helm install k2-agent . --set secrets.K2_MAILBOX_ID="$MAILBOX_ID" --set secrets.K2_MANAGER_URL="$MANAGER_URL" || true
+      sleep 10
+      if kubectl --namespace k2view-agent get deploy k2view-agent | grep -q "k2view-agent   1/1"
+      then
+        print_colored_bold "green" '√ K2-Agent Deployed successfully ...'
+      else
+        print_colored_bold "red" 'x K2-Agent Deployment failed ...'
+      fi
+      ;;
+  esac 
+
+}
+
+clear
+cat ./static/logo.ans
+init
+echo ; echo
+
+print_colored_bold "magenta" "The following tools will be installed:"
+print_colored_bold "white" " "
+echo "
+|Name            |Required |More info                                   |
+|----------------|---------|--------------------------------------------|
+|Microk8s        |Yes      |'https://microk8s.io/'                      |
+|Git             |Yes      |'https://git-scm.com/'                      |
+|cert-manager    |Yes      |'https://cert-manager.io/'                  |
+|NGINX Ingress   |Yes      |'https://docs.nginx.com/'                   |
+|hostpath-storage|Yes      |                                            |
+|docker registry |Yes      |'https://microk8s.io/docs/registry-built-in'|                           |
+|metrics-server  |Yes      |                                            |
+|k2-agent        |Yes      |'helm/charts/k2view-agent'                  |
+"  
+
+if ! confirm "Installing Kubernetes Components"
+then
+    echo "Exiting ..."
+    exit 1
+else
+  if ! git version >/dev/null 
+  then
+    install_git
+  fi
+  install_microk8s
+  configure_microk8s
+  install_k2agent
+fi
