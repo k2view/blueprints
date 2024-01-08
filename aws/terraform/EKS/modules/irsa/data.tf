3df566ba7d977a4291c3b288f00d90172bb74e2b
Itay Almani <itay.almani@k2view.com>
2024-01-08 16:40:54 +0200

    add sa impl with annotations

--- aws/terraform/EKS/modules/irsa/data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_eks_cluster" "eks_cluster" {
-  name = var.cluster_name
-}
-
-data "tls_certificate" "tls" {
-  url = data.aws_eks_cluster.eks_cluster.identity.0.oidc.0.issuer
-}
-
-data "aws_caller_identity" "current" {}
