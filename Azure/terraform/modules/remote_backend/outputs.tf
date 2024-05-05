cf8a891d5503da67a900badc8a5a20df6e8d9d8a
itayAlmani1 <itay.almani@k2view.com>
2024-05-05 14:34:12 +0300

    remove remote_backend from azure modules folder

--- Azure/terraform/modules/remote_backend/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "storage_account_name" {
-  value         = azurerm_storage_account.tfstate_storage.name
-  description   = "The name of the Azure Storage Account used to store Terraform states."
-}
