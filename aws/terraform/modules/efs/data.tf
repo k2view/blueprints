6c26ee8052f34417b4057b7c0eb5b699b9b115cc
Vladimir Startsev <vstartse@k2view.com>
2024-07-31 11:41:09 +0300

    delete old modules

--- aws/terraform/modules/efs/data.tf
+++
@@ -1,5 +0,0 @@
-data "aws_eks_cluster" "example" {
-  name = var.cluster_name
-}
-
-data "aws_caller_identity" "current" {}
--- aws/terraform/modules/efs/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "efs_id" {
-  value = aws_efs_file_system.EKS_EFS.id
-}
-
-output "efs_driver_SA_arn"{
-  value = aws_iam_role.AmazonEKS_EFS_CSI_DriverRole.arn
-}
