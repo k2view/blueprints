4f475ed78fcdd26b5ff8ca12c11f346043335556
Vladimir Startsev <vstartse@k2view.com>
2024-08-13 10:06:36 +0300

    remove deprecated module Route53 in aws

--- aws/terraform/EKS/modules/Route53/subdomain-hz/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "hz_zone_id" {
-  value       = aws_route53_zone.cluster_domain.id
-  description = "Route 53 hosted zone ID."
-}
-
-output "cert_arn" {
-  value       = aws_acm_certificate.cert.arn
-  description = "ACM certificate ARN."
-}
